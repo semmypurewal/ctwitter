fe47792b76372529bc84570b656add017b6975e0
Semmy Purewal <semmypurewal@gmail.com>
2012-02-26 15:14:39 -0500

    complete start method spec and implementation

--- spec/jsonp_poller_spec.js
+++ spec/jsonp_poller_spec.js
@@ -16,7 +16,7 @@ describe('JSONP Poller', function() {
 
     it('implements the JSONP Poller interface', function() {
 	var i;
-	var interface = ['url', 'start', 'stop', 'process', 'isPolling'];
+	var interface = ['url', 'start', 'stop', 'process', 'timeout', 'isPolling', 'name'];
 	for(i = 0; i < interface.length; i++) {
 	    expect(jp[interface[i]]).not.toBeUndefined();
 	    expect(typeof(jp[interface[i]]) === 'function').toBeTruthy();
@@ -24,22 +24,8 @@ describe('JSONP Poller', function() {
     });
 
     describe('url method', function() {
-	it('throws an error if url is called without url being set previously', function() {
-	    var badCall = function() {
-		var u = jp.url();
-	    };
-	    expect(badCall).toThrow(new Error('url needs to be set before you call it as a getter'));
-	});
-
 	it('accepts a string that represents the url', function() {
 	    jp.url('http://www.thisisajsonfeed.com/feed.json');
-	});
-
-	it('throws an error on a non-string argument', function() {
-	    var badCall = function() {
-		jp.url(5);
-	    };
-	   expect(badCall).toThrow(new Error('url only accepts a string argument'));
 	});
 
 	it('returns a string that represents the current url it is polling', function() {
@@ -47,15 +33,99 @@ describe('JSONP Poller', function() {
 	    var u = jp.url();
 	    expect(u).toEqual('http://www.thisisajsonfeed.com/feed.json');
 	});
+
+	it('throws an error on a non-string argument', function() {
+	    var badCall = function() {
+		jp.url(5);
+	    };
+	    expect(badCall).toThrow(new Error('url only accepts a string argument'));
+	});
+
+	it('throws an error if url is called without url being set previously', function() {
+	    var badCall = function() {
+		var u = jp.url();
+	    };
+	    expect(badCall).toThrow(new Error('url needs to be set before you call it as a getter'));
+	});
+    });
+
+    describe('timeout method', function() {
+	it('defaults the timeout to 0', function() {
+	    expect(jp.timeout()).toBe(0);
+	});
+
+	it('accepts an integer that represents the timeout in seconds and stores it in timeout', function() {
+	    jp.timeout(30);
+	    expect(jp.timeout()).toBe(30);
+	});
+
+	it('throws an error on a non-numeric parameter', function() {
+	    var badCall = function() {
+		jp.timeout('hello');
+	    };
+	    expect(badCall).toThrow(new Error('timeout requires the parameter to be an integer'));
+	});
+    });
+
+    describe('name method', function() {
+	it('returns a string that is the name of this poller', function() {
+	    expect(typeof(jp.name()) === 'string').toBeTruthy();
+	});
     });
 
     describe('start method', function() {
-	xit('starts polling', function() {
-
+	beforeEach(function() {
+	    jp.url("fixtures/public_timeline.json");
+	    jp.start();
 	});
 
-	xit('adds a script tag to the head immediately', function() {
+	it('starts polling', function() {
+	    expect(jp.isPolling()).toBeTruthy();
+	});
 
+	it('adds a script tag with URL to DOM', function() {
+	    var script = document.getElementById(jp.name()+"_script_tag_id");
+	    expect(script.src.match(new RegExp(jp.url().substring(0,jp.url().indexOf('?'))))).toBeTruthy();
+	});
+
+	it('replaces the % in the callback with the name of this process function', function() {
+	    var jp2 = new JSONPPoller();
+	    jp2.url("fixtures/public_timeline.json?callback=%");
+	    jp2.start();
+
+	    var script = document.getElementById(jp2.name()+"_script_tag_id");
+	    expect(script.src.match(new RegExp("callback="+jp2.name()+".process"))).toBeTruthy();
+	});
+
+	it('removes previous script tag from the DOM if it exists', function() {
+	    var scripts = [];
+	    var script_tags;
+	    var i;
+	    jp.start();
+	    scripts = [];
+	    script_tags = document.getElementsByTagName('script');
+	    for(i = 0; i < script_tags.length; i++) {
+		if(script_tags[i].id !== 'undefined' && script_tags[i].id.match(jp.name())) {
+		    scripts.push(script_tags[i]);
+		};
+	    };
+	    expect(scripts.length).toBe(1);
+	});
+
+	xit('reloads and processes the data after the specified timeout', function() {
+	    jp.timeout(3);
+	    jp.start();
+	    waitsFor(function() {
+		//not sure how to test this yet
+	    }, "start never called a second time", 5000);
+	});
+
+	it('throws error if no URL has been specified', function() {
+	    var jp2 = new JSONPPoller();
+	    var badCall = function() {
+		jp2.start();
+	    };
+	    expect(badCall).toThrow(new Error('start requires a url to have been specified'));
 	});
     });
 
@@ -74,11 +144,21 @@ describe('JSONP Poller', function() {
 
 	});
 
+	xit('is called after polling starts and data is returned from the URL', function() {
+	    var processStub = jasmine.createSpy();
+	});
+
 	xit('accepts a data object and then processes it', function() {
 
 	});
 
-	xit('is called when data is emitted', function() {
+	xit('emits data when new data is available', function() {
+
+	});
+
+
+
+	xit('emits error if an error is returned', function() {
 
 	});
 
@@ -92,17 +172,19 @@ describe('JSONP Poller', function() {
     });
 
     describe('isPolling method', function() {
-	xit('returns false if it has not started', function() {
+	it('returns false if it has not started', function() {
 	    expect(jp.isPolling()).toBeFalsy();
 	});
 
-	xit('returns true if it has started', function() {
+	it('returns true if it has started', function() {
+	    jp.url("fixtures/public_timeline.json");
 	    jp.start();
 	    expect(jp.isPolling()).toBeTruthy();
 	});
 
-	xit('toggles true and false between calls', function() {
+	it('toggles true and false between calls', function() {
 	    expect(jp.isPolling()).toBeFalsy();
+	    jp.url("fixtures/public_timeline.json");
 	    jp.start();
 	    expect(jp.isPolling()).toBeTruthy();
 	    jp.stop();
--- src/jsonp_poller.js
+++ src/jsonp_poller.js
@@ -1,8 +1,16 @@
 define(function(require, exports, module) {
     require(["event_emitter"], function(EventEmitter) {
 	JSONPPoller.prototype = new EventEmitter();
+	JSONPPoller.instanceCount = 0;
 	function JSONPPoller() {
 	    var url;
+	    var timer = null;
+	    var polling = false;
+	    var timeout = 0;
+	    JSONPPoller.instanceCount++;
+	    var prefix = "__jp_";
+	    var name = prefix+JSONPPoller.instanceCount;
+
 	    this.emits(['error','data']);
 
 	    this.url = function(u) {
@@ -17,20 +25,59 @@ define(function(require, exports, module) {
 		}
 	    };
 
-	    this.start = function() {
-
+	    this.name = function() {
+		return name;
 	    };
 
-	    this.stop = function() {
+	    this.start = function() {
+		try {
+		    this.url();
+		} catch(e) {
+		    throw new Error('start requires a url to have been specified');
+		}
 
+		polling = true;
+		var thisPoller = this;
+		
+		var head = document.getElementsByTagName('head')[0]; //assuming there is only 1 head?
+		var script = document.getElementById(this.name()+'_script_tag_id');
+		if(script) {
+		    head.removeChild(script);
+		};
+		script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.id = this.name()+'_script_tag_id';
+		script.src = this.url().replace('=%',"="+this.name()+'.process');
+		head.appendChild(script);
+
+		if(this.timeout() > 0) {
+		    timer = setTimeout(function() {
+			thisPoller.start();
+		    }, thisPoller.timeout()*1000);
+		};
+	    };
+
+
+	    this.stop = function() {
+		polling = false;
 	    };
 
 	    this.process = function() {
 
 	    };
 
-	    this.isPolling = function() {
+	    this.timeout = function(t) {
+		if(t === undefined) {
+		    return timeout;
+		} else if(typeof(t) !== 'number') {
+		    throw new Error('timeout requires the parameter to be an integer');
+		} else {
+		    timeout = t;
+		};
+	    };
 
+	    this.isPolling = function() {
+		return polling;
 	    };
 	};
 
