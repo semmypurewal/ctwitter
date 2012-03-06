499d4e9769542edec6a0ba7efe79a2709886da7c
Semmy Purewal <semmypurewal@gmail.com>
2012-03-06 12:23:42 -0500

    namespace package

--- src/ctwitter.js
+++ src/ctwitter.js
@@ -1,82 +1,96 @@
-function CTwitter() {
-  
-    /**
-     * stream
-     * should accept mode, options and callback
-     * options argument is, of course, optional
-     * deliver data to the client one tweet at a time
-     * throws error on
-     *   --mode not a string
-     *   --callback not a function
-     *   --no callback
-     *   --no mode
-     */
-    this.stream = function(mode, options, callback) {
-	var twitterPoller = new JSONPPoller()
-	, stream = new EventEmitter()
-	, buffer = []
-	, bufferTimeout
-	, deliverData = function(stream) {
-	    stream.emit('data',buffer.shift());
-	    if(buffer.length > 0) {
-		setTimeout(function() {
-		    deliverData(stream);
-		}, 750);
-	    }
-	};
-
-	if(arguments.length === 2) {
-	    callback = arguments[1];
-	    options = null;
-	}
-	
-	if(typeof(mode) !== 'string') {
-	    throw new Error('stream requires mode and it must be a string');
-	} else if(typeof(callback) !== 'function') {
-	    throw new Error('stream requires callback and it must be a function');
-	} else if(options !== null && typeof(options) !== 'object') {
-	    throw new Error('stream requires options parameter to be an object');
-	}
-	
-	stream.emits(['data','error','destroy']);
-	stream.destroy = function() { stream.emit('destroy'); };
-	stream.on('destroy', function() {
-	    twitterPoller.stop();
-	    clearTimeout(bufferTimeout);
-	});
-
-	//process mode
-	
-	//process options
-	
-	twitterPoller.url('http://search.twitter.com/search.json?q=bieber&callback=%').timeout(25).process(function(data) {
-	    var i
-	    , nextUrl;
-
-	    //data = data.results;
-
-	    //check frequency of data to set up bufferTimeout
-	    //and next polling timeout
-	    
-	    //set up buffer to deliver data
-	    for(i = 0; i < data.results.length; i=i+1) {
-		buffer.push(data.results[i]);
-	    }
-
-	    //deliver data
-	    if(buffer.length > 0) {
-		deliverData(stream);
-	    }
-    
-	    //update poller and timeout for next request
-	    nextUrl = 'http://search.twitter.com/search.json' + data.refresh_url + '&callback=%';
-	    twitterPoller.url(nextUrl).timeout(25);
-	});
-
-	//set up the poller as specified by the client
-	callback(stream);
-	
-	//start the poller
-	twitterPoller.start();
-    };
+if(!window.ctwitter || !window.ctwitter.JSONPPoller || !window.ctwitter.EventEmitter) {
+    throw new Error('ctwitter requires JSONPPoller and EventEmitter');
 }
+(function(package) {
+    //EventEmitter && JSONPPoller should already be in package
+    var EventEmitter = package.EventEmitter
+    , JSONPPoller = package.JSONPPoller;
+
+    function CTwitter() {
+	/**
+	 * stream
+	 * should accept mode, options and callback
+	 * options argument is, of course, optional
+	 * deliver data to the client one tweet at a time
+	 * throws error on
+	 *   --mode not a string
+	 *   --callback not a function
+	 *   --no callback
+	 *   --no mode
+	 */
+	this.stream = function (mode, options, callback) {
+            var twitterPoller = new JSONPPoller()
+	    , timeout = 25
+            , stream = new EventEmitter()
+            , buffer = []
+            , bufferTimeout
+	    , lastID
+	    , isStreaming = false
+            , deliverData = function (stream) {
+		isStreaming = true;
+		stream.emit('data',buffer.shift());
+		if (buffer.length > 0) {
+		    console.log(lastID < buffer[0].id_str);
+		    lastID = buffer[0].id_str;
+                    bufferTimeout = setTimeout(function () {
+			deliverData(stream);
+                    }, 750);
+		} else {
+		    isStreaming = false;
+		}
+            };
+	    
+            if (arguments.length === 2) {
+		callback = arguments[1];
+		options = null;
+            }
+        
+            if (typeof (mode) !== 'string') {
+		throw new Error('stream requires mode and it must be a string');
+            } else if (typeof (callback) !== 'function') {
+		throw new Error('stream requires callback and it must be a function');
+            } else if (options !== null && typeof (options) !== 'object') {
+		throw new Error('stream requires options parameter to be an object');
+            }
+
+            stream.emits(['data', 'error', 'destroy']);
+            stream.destroy = function () { stream.emit('destroy'); };
+            stream.on('destroy', function () {
+		twitterPoller.stop();
+		clearTimeout(bufferTimeout);
+            });
+
+            //TODO: process mode
+            //TODO: process options
+        
+            twitterPoller.url('http://search.twitter.com/search.json?rpp=100&q=bieber&callback=%').timeout(timeout).process(function (data) {
+		var i
+		, nextUrl;
+		
+		//TODO: check frequency of data to set up bufferTimeout and next polling timeout??
+		
+		//set up buffer to deliver data
+		for (i = data.results.length - 1; i > 0; i = i - 1) {
+                    buffer.push(data.results[i]);
+		}
+
+		//deliver data if it's not already being delivered
+		if (!isStreaming) {
+                    deliverData(stream);
+		}
+
+		//update poller and timeout for next request
+		nextUrl = 'http://search.twitter.com/search.json?rpp=100&result_type=recent' + data.refresh_url + '&callback=%';
+		twitterPoller.url(nextUrl).timeout(timeout);
+            });
+
+            //set up the poller as specified by the client
+            callback(stream);
+	    
+            //start the poller
+            twitterPoller.start();
+	};
+    }
+
+    package.CTwitter = CTwitter;
+})(window.ctwitter);
--- src/jsonp_poller.js
+++ src/jsonp_poller.js
@@ -1,170 +1,177 @@
-function JSONPPoller() {
-    var url
-    , count = 0 //number of requests made
-    , timer = null //timeout for next request
-    , polling = false
-    , timeout = 0
-    , processor = function(data) { return {update:true, data:data}; } //default process implementation
-    , prefix = "__jp_"
-    , name;
+if(!window.ctwitter || !window.ctwitter.EventEmitter) {
+    throw new Error('JSONPPoller requires EventEmitter');
+}
+(function(package) {
+    var EventEmitter = package.EventEmitter;
+    function JSONPPoller() {
+	var url
+	, count = 0 //number of requests made
+	, timer = null //timeout for next request
+	, polling = false
+	, timeout = 0
+	, processor = function(data) { return {update:true, data:data}; } //default process implementation
+	, prefix = "__jp_"
+	, name;
 
-    JSONPPoller.instanceCount = JSONPPoller.instanceCount+1;
-    name = prefix+JSONPPoller.instanceCount;
-    window[name] = this;
-    
-    this.emits(['error','data']);
-    
-    /**
-     * url setter/getter
-     * accepts a string that sets the URL of the JSONP feed
-     * returns the current URL if called with no parameter
-     * throws an error if:
-     *   --non-string argument
-     *   --called as a getter before the url is set
-     */
-    this.url = function(u) {
-	var result = this; //for chaining
-	if(u === undefined && url === undefined) {
-	    throw new Error('url needs to be set before you call it as a getter');
-	} else if(u === undefined) {
-	    result = url;
-	} else if(typeof(u) !== 'string') {
-	    throw new Error('url only accepts a string argument');
-	} else {
-	    url = u;
-	}
-	return result;
-    };
-    
-    /**
-     * name
-     * returns the global name of this object
-     */
-    this.name = function() {
-	return name;
-    };
-    
-    /**
-     * start
-     * starts polling
-     * adds a script with the URL to the DOM
-     * replaces callback=% with the actual callback based on the name
-     * removes the previous script tag if it exists
-     * throws error if
-     *   --url has not been specified
-     */
-    this.start = function() {
-	var thisPoller = this
-	, head = document.getElementsByTagName('head')[0] //assuming there is only 1 head?
-	, script = document.getElementById(this.name()+'_script_tag_id');
-	try {
-	    this.url();
-	} catch(e) {
-	    throw new Error('start requires a url to have been specified');
-	}
+	JSONPPoller.instanceCount = JSONPPoller.instanceCount+1;
+	name = prefix+JSONPPoller.instanceCount;
+	window[name] = this;
 	
-	polling = true;
-	count=count+1;
-
-	if(script) {
-	    head.removeChild(script);
-	}
-	script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.id = this.name()+'_script_tag_id';
-	script.src = this.url().replace('=%',"="+this.name()+'.process');
-	head.appendChild(script);
-    };
+	this.emits(['error','data']);
     
-    /**
-     * stop
-     * stops polling
-     * cancels the next call to start
-     * removes script tag from head
-     */
-    this.stop = function() {
-	var script = document.getElementById(this.name()+'_script_tag_id')
-	, head;
-	polling = false;
-	if(timer) {
-	    clearTimeout(timer);
-	}
-
-	if(script !== null) {
-	    head = document.getElementsByTagName('head')[0];
-	    head.removeChild(script);
-	}
-    };
+	/**
+	 * url setter/getter
+	 * accepts a string that sets the URL of the JSONP feed
+	 * returns the current URL if called with no parameter
+	 * throws an error if:
+	 *   --non-string argument
+	 *   --called as a getter before the url is set
+	 */
+	this.url = function(u) {
+	    var result = this; //for chaining
+	    if(u === undefined && url === undefined) {
+		throw new Error('url needs to be set before you call it as a getter');
+	    } else if(u === undefined) {
+		result = url;
+	    } else if(typeof(u) !== 'string') {
+		throw new Error('url only accepts a string argument');
+	    } else {
+		url = u;
+	    }
+	    return result;
+	};
     
-    //method only used for testing
-    //keeps track of the number of times the
-    //URL has been polled
-    this.count = function() {
-	return count;
-    };
+	/**
+	 * name
+	 * returns the global name of this object
+	 */
+	this.name = function() {
+	    return name;
+	};
     
-    /**
-     * process
-     * accepts a function as an argument and sets it to the pre-processor
-     * accepts an object as an argument and processes it, emitting new data
-     *   if it is available
-     * reloads and reprocesses the data after the timeout
-     * throws an error if
-     *   --argument is not a function or an object
-     */
-    this.process = function(f) {
-	var processorResult
-	, thisPoller = this
-	, result = thisPoller;
-	if(typeof(f) === 'function') {
-	    processor = f;
-	} else if (typeof(f) === 'object') {
-	    //do object stuff
-	    processorResult = processor(f);
-	    if(processorResult && processorResult.error) {
-		this.emit('error', processorResult.error);
-	    } else if(processorResult && processorResult.update) {
-		this.emit('data', processorResult.data);
+	/**
+	 * start
+	 * starts polling
+	 * adds a script with the URL to the DOM
+	 * replaces callback=% with the actual callback based on the name
+	 * removes the previous script tag if it exists
+	 * throws error if
+	 *   --url has not been specified
+	 */
+	this.start = function() {
+	    var thisPoller = this
+	    , head = document.getElementsByTagName('head')[0] //assuming there is only 1 head?
+	    , script = document.getElementById(this.name()+'_script_tag_id');
+	    try {
+		this.url();
+	    } catch(e) {
+		throw new Error('start requires a url to have been specified');
+	    }
+	
+	    polling = true;
+	    count=count+1;
+	    
+	    if(script) {
+		head.removeChild(script);
+	    }
+	    script = document.createElement('script');
+	    script.type = 'text/javascript';
+	    script.id = this.name()+'_script_tag_id';
+	    script.src = this.url().replace('=%',"="+this.name()+'.process');
+	    head.appendChild(script);
+	};
+    
+	/**
+	 * stop
+	 * stops polling
+	 * cancels the next call to start
+	 * removes script tag from head
+	 */
+	this.stop = function() {
+	    var script = document.getElementById(this.name()+'_script_tag_id')
+	    , head;
+	    polling = false;
+	    if(timer) {
+		clearTimeout(timer);
 	    }
 	    
-	    //set up the next request
-	    if(this.timeout() > 0) {
-		timer = setTimeout(function() {
-		    thisPoller.start();
-		}, thisPoller.timeout()*1000);
+	    if(script !== null) {
+		head = document.getElementsByTagName('head')[0];
+		head.removeChild(script);
 	    }
-	} else {
-	    throw new Error('process requires the parameter to be an object or a function');
-	}
-	return result;
-    };
+	};
     
-    /**
-     * timeout
-     * the default timeout is set to 0
-     * accepts an integer that represents a timeout and stores it in timeout
-     * returns the timeout when called with no parameters
-     * throws error on:
-     *   --non numeric parameter
-     */
-    this.timeout = function(t) {
-	var result = this;
-	if(t === undefined) {
-	    result = timeout;
-	} else if(typeof(t) !== 'number') {
-	    throw new Error('timeout requires the parameter to be an integer');
-	} else {
-	    timeout = t;
-	    if(timeout === 0 && polling) {
-		this.stop();
+	//method only used for testing
+	//keeps track of the number of times the
+	//URL has been polled
+	this.count = function() {
+	    return count;
+	};
+    
+	/**
+	 * process
+	 * accepts a function as an argument and sets it to the pre-processor
+	 * accepts an object as an argument and processes it, emitting new data
+	 *   if it is available
+	 * reloads and reprocesses the data after the timeout
+	 * throws an error if
+	 *   --argument is not a function or an object
+	 */
+	this.process = function(f) {
+	    var processorResult
+	    , thisPoller = this
+	    , result = thisPoller;
+	    if(typeof(f) === 'function') {
+		processor = f;
+	    } else if (typeof(f) === 'object') {
+		//do object stuff
+		processorResult = processor(f);
+		if(processorResult && processorResult.error) {
+		    this.emit('error', processorResult.error);
+		} else if(processorResult && processorResult.update) {
+		    this.emit('data', processorResult.data);
+		}
+		
+		//set up the next request
+		if(this.timeout() > 0) {
+		    timer = setTimeout(function() {
+			thisPoller.start();
+		    }, thisPoller.timeout()*1000);
+		}
+	    } else {
+		throw new Error('process requires the parameter to be an object or a function');
 	    }
-	}
-	return result;
-    };
+	    return result;
+	};
     
-    this.isPolling = function() {
-	return polling;
-    };
-}
-JSONPPoller.prototype = new EventEmitter();
-JSONPPoller.instanceCount = 0;
+	/**
+	 * timeout
+	 * the default timeout is set to 0
+	 * accepts an integer that represents a timeout and stores it in timeout
+	 * returns the timeout when called with no parameters
+	 * throws error on:
+	 *   --non numeric parameter
+	 */
+	this.timeout = function(t) {
+	    var result = this;
+	    if(t === undefined) {
+		result = timeout;
+	    } else if(typeof(t) !== 'number') {
+		throw new Error('timeout requires the parameter to be an integer');
+	    } else {
+		timeout = t;
+		if(timeout === 0 && polling) {
+		    this.stop();
+		}
+	    }
+	    return result;
+	};
+	
+	this.isPolling = function() {
+	    return polling;
+	};
+    }
+    JSONPPoller.prototype = new EventEmitter();
+    JSONPPoller.instanceCount = 0;
+    package.JSONPPoller = JSONPPoller;
+})(window.ctwitter);
